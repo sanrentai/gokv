e26970e71eee7723aa5870fd1e68923559514f1f
sanrentai <sanrentai@163.com>
2023-07-29 14:06:49 +0800

    简化代码，仅保留必要功能

--- cmd/main.go
+++ cmd/main.go
@@ -7,31 +7,34 @@ import (
 )
 
 func main() {
-	db, err := gokv.NewKVDB("d:/gokv")
+	db, err := gokv.NewKVDB("db.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	err = db.Put("key1", "value1")
-	if err != nil {
-		log.Fatal(err)
+	// err = db.Put("key1", "value1")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// err = db.Put("key2", "value2")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	db.Delete("key1")
+
+	value, ok := db.Get("key1")
+	if !ok {
+		log.Println("不存在key1")
+	} else {
+		log.Println("Value:", value)
 	}
 
-	err = db.Put("key2", "value2")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	value, err := db.Get("key1")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Value:", value)
-
-	value, err = db.Get("key2")
-	if err != nil {
-		log.Fatal(err)
+	value, ok = db.Get("key2")
+	if !ok {
+		log.Println("不存在key2")
 	}
 	log.Println("Value:", value)
 }
